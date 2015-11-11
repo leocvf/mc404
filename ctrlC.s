9df6a52a04090ae09b9abf49d616efed477f9f47
Otavio Miranda <ovm357@gmail.com>
2015-11-11 15:26:44 -0200

    ctrlC

--- ctrlC.s
+++ ctrlC.s
@@ -1,1 +1,20 @@
-c c
+source /home/specg12-2/mc404/simulador/set_path.sh
+arm-eabi-as -g ra158319.s -o ra158319.o
+arm-eabi-ld ra158319.o -o ra158319 -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801800 -e 0x778005e0
+mksd.sh --so ra158319 --user faz_nada
+arm-sim --rom=/home/specg12-1/mc404/simulador/simulador_player/bin/dumboot.bin --sd=disk.img -g
+
+arm-eabi-as -g faz_nada.s -o faz_nada.o
+arm-eabi-ld faz_nada.o -o faz_nada -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801800 -e 0x778005e0
+
+source /home/specg12-2/mc404/simulador/set_path.sh
+arm-eabi-gdb ra158319
+
+target remote localhost:5000
+b ra158319.s:105
+command
+p * (int) 0x77801800
+c
+end
+c
+
