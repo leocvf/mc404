26c8edfc026e65b19f4974815ace3a9ed4ee05c1
Otavio Miranda <ovm357@gmail.com>
2015-11-13 12:20:50 -0200

    update 12:20 13/11/15

--- ctrlC.s
+++ ctrlC.s
@@ -1,4 +1,5 @@
 source /home/specg12-2/mc404/simulador/set_path.sh
+
 arm-eabi-as -g soul.s -o soul.o
 arm-eabi-ld soul.o -o soul -g --section-start=.iv=0x778005e0 -Ttext=0x77800700 -Tdata=0x77801800 -e 0x778005e0
 
@@ -9,11 +10,21 @@ arm-eabi-ld faz_nada.o -o faz_nada -g --section-start=.iv=0x778005e0 -Ttext=0x77
 mksd.sh --so soul --user faz_nada
 arm-sim --rom=/home/specg12-1/mc404/simulador/simulador_player/bin/dumboot.bin --sd=disk.img -g
 
+
+
 source /home/specg12-2/mc404/simulador/set_path.sh
 arm-eabi-gdb soul
 
 target remote localhost:5000
-b soul.s:209
+b soul.s:95
+b soul.s:99
+b soul.s:103
+b soul.s:112
+b soul.s:132
+b soul.s:137
+b soul.s:157
+b soul.s:200
+b soul.s:210
 command
 p * (int) 0x77801800
 c
